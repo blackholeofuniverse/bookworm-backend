126f6f7f0bc29a38b91709c13955d7a55b2e2258
blackholeofuniverse <omb4505@gmail.com>
2025-03-22 16:06:35 +0530

    implemented cron job

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import authRoutes from './routes/auth.route.js';
 import bookRoutes from './routes/book.route.js';
 import connectToDb from './lib/db.js';
 import cors from "cors"
+import job from "./lib/cron.js"
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+job.start()
 app.use(express.json())
 app.use(cors())
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
+
+app.get("/", (_req, res) => {
+    res.json({message: "Server is running"})
+})
 
 
 app.listen(PORT, () => {
